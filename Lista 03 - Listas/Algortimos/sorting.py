e89fd99007da2807cf702d5f0c477e9476822067
gcgc <gcgc@g1c08.cin.ufpe.br>
2025-10-13 17:02:19 -0300

    upddate(lista-3):finalizando-q-1

---
+++ Lista 03 - Listas/Algortimos/sorting.py
@@ -0,0 +1,30 @@
+# Vetor base para o ordenamento decrescente
+import random
+vet2D = [
+         [5, 6, 4, 7, 5, 1, 7, 2, 6],
+         [y for y in ["cabeça", "morango", "morango", "ovelha", "morango", "morango", "cabeça", "morango", "morango"]]   
+        ]
+print(vet2D)
+
+for i in range(len(vet2D[0])):
+    minValue = min(vet2D[0][i:])
+    idmin = vet2D[0].index(minValue, i)
+    if (vet2D[0][idmin] < vet2D[0][i]):
+        vet2D[0][i], vet2D[0][idmin] = vet2D[0][idmin], vet2D[0][i]
+        vet2D[1][i], vet2D[1][idmin] = vet2D[1][idmin], vet2D[1][i]
+
+print(vet2D)
+
+
+# Ordenamento lexográfico pós sorting geral
+lenVectWord, currentWord = (len(vet2D[1]) - 1), 0
+while (currentWord < lenVectWord):
+    if (vet2D[0][currentWord] == vet2D[0][currentWord + 1]): # Verifica condição de empate entre pontuação
+        findLowest, length, letter = False, min(len(vet2D[1][currentWord]), len(vet2D[1][currentWord+1])), 0 # Condição crescente de lexografia
+        while (letter < length and not findLowest): # Varre as palavras
+            if vet2D[1][currentWord][letter] > vet2D[1][currentWord + 1][letter]:
+                findLowest, vet2D[1][currentWord], vet2D[1][currentWord + 1] = True, vet2D[1][currentWord + 1], vet2D[1][currentWord]
+            letter += 1
+    currentWord += 1
+
+print(vet2D)
--- Lista 03 - Listas/O-Churrasco-Epico-do-Calabreso.py
+++ Lista 03 - Listas/O-Churrasco-Epico-do-Calabreso.py
@@ -5,8 +5,8 @@ comida mais cara - reconhecimento especial
 comida mais barata - feeback constrututivo
 '''
 
-numConvidados, nameConvidados, vet2Comidas  = int(input()), [], [ [], # Nome das Comidas
-                                                                  []  # Valor das Comidas
+numConvidados, nameConvidados, vet2Comidas  = int(input()), [], [ [], # Valor das Comidas
+                                                                  []  # Nome das Comidas
                                                                 ]
 
 for convidado in range(numConvidados):
@@ -18,21 +18,52 @@ for convidado in range(numConvidados):
     if ("Maicon Kuster" == name):
         print(f"você é convidado DE GUÊÊ???, sai da minha festa seu FOFOQUEIRO!!")
     # Verifica se é um prato repetido
-    elif (nameDish in vet2Comidas[0]):
+    elif (nameDish in vet2Comidas[1]):
         print(f"Na Festa do Calabreso não pode comida Repetida SAI FORA {name}")
     else:
         nameConvidados.append(name)
-        vet2Comidas[0].append(nameDish)
-        vet2Comidas[1].append(value)
-
+        vet2Comidas[0].append(value)
+        vet2Comidas[1].append(nameDish)
 
 # Caso niguém vá para a festa do calabreso
 if (len(nameConvidados) == 0): print(f"Nenhum convidado marcou presença na festa do calabreso!")
 elif (len(nameConvidados) == 1): print(f"Obrigado para o(a) {nameConvidados[0]} pelo(a) excelente {vet2Comidas[0][0]}")
 else:
-    idGrastestValue, idLowestValue = vet2Comidas[1].index(max(vet2Comidas[1])), vet2Comidas[1].index(min(vet2Comidas[1]))
+    idGrastestValue, idLowestValue = vet2Comidas[0].index(max(vet2Comidas[0])), vet2Comidas[0].index(min(vet2Comidas[0]))
     # Sort dos valores
     # Saída da pessoa que trouxe a comida mais cara
-    print(f"Obrigado para o(a) {nameConvidados[idGrastestValue]} pelo(a) excelente {vet2Comidas[0][idGrastestValue]}")
+    print(f"Obrigado para o(a) {nameConvidados[idGrastestValue]} pelo(a) excelente {vet2Comidas[1][idGrastestValue]}")
     # Saída da pessoa que trouxe a comida mais barata
-    print(f"Rapaz, {nameConvidados[idLowestValue]} trouxe o(a) {vet2Comidas[0][idLowestValue]} que estava altamente mais ou menos!!!")
+    print(f"Rapaz, {nameConvidados[idLowestValue]} trouxe o(a) {vet2Comidas[1][idLowestValue]} que estava altamente mais ou menos!!!")
+
+
+# Sorting
+
+for i in range(len(vet2Comidas[0])):
+    minValue = min(vet2Comidas[0][i:])
+    idmin = vet2Comidas[0].index(minValue, i)
+    if (vet2Comidas[0][idmin] < vet2Comidas[0][i]):
+        vet2Comidas[0][i], vet2Comidas[0][idmin] = vet2Comidas[0][idmin], vet2Comidas[0][i]
+        vet2Comidas[1][i], vet2Comidas[1][idmin] = vet2Comidas[1][idmin], vet2Comidas[1][i]
+        nameConvidados[i], nameConvidados[idmin] = nameConvidados[idmin], nameConvidados[i]
+
+
+# Ordenamento lexográfico pós sorting geral
+lenVectWord, currentWord = (len(nameConvidados) - 1), 0
+while (currentWord < lenVectWord):
+    if (vet2Comidas[0][currentWord] == vet2Comidas[0][currentWord + 1]): # Verifica condição de empate entre pontuação
+        findLowest, length, letter = False, min(len(nameConvidados[currentWord]), len(nameConvidados[currentWord+1])), 0 # Condição crescente de lexografia
+        while (letter < length and not findLowest): # Varre as palavras
+            if nameConvidados[currentWord][letter] > nameConvidados[currentWord + 1][letter]:
+                findLowest, nameConvidados[currentWord], nameConvidados[currentWord + 1] = True, nameConvidados[currentWord + 1], nameConvidados[currentWord]
+                vet2Comidas[1][currentWord], vet2Comidas[1][currentWord + 1] = vet2Comidas[1][currentWord + 1], vet2Comidas[1][currentWord]
+            elif (nameConvidados[currentWord][letter] < nameConvidados[currentWord + 1][letter]):
+                findLowest = True
+            letter += 1
+    currentWord += 1
+
+
+# Output final
+print(f"Lista de convidados do Calabreso")
+for i in range(lenVectWord + 1):
+    print(f"{i + 1}- {nameConvidados[i]}")
