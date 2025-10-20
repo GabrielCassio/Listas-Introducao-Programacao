e57985593140c3c1ea37f01192decf8951da455f
Gabriel Cassio Gomes Cileiro <gcgc@WINDOWS.CIN.UFPE.BR>
2025-10-20 16:58:31 -0300

    update(lista-3):teminando-q10

--- Lista 03 - Listas/Jamal-no-CInbebeda.py
+++ Lista 03 - Listas/Jamal-no-CInbebeda.py
@@ -1,5 +1,20 @@
 nmMonitors = ["Júnior", "Henrique", "Miguel", "Guilherme"]
 entry = input().split(", ")
+nomes, notas = entry[0:-1:2], [int(i) for i in entry[1:len(entry):2]]
+print(notas)
 
-if ((not nmMonitors[0] in entry) or (not nmMonitors[1] in entry) or (not nmMonitors[2] in entry) or (not nmMonitors[3] in entry)):
-    ...
+# Verifica se todos os monitores desejados
+for i in range(len(nomes)):
+    while (not nomes[i] in nmMonitors):
+        #print(f"O nome {nomes[i]} não é de um monitor.")
+        nomes[i] = input()
+
+#print(f"Todos os nomes são de monitores 👍")
+# --------------------------------------
+
+indexMinValue = notas.index(min(notas))
+movPes = input().split(", ")
+
+# Verificação dos Movimentos (E ou D) | Tem 7 movimentos
+if (len(movPes) != 7):
+    pass
